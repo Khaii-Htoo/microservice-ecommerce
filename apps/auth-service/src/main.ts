7cd1cff0aeae37acb01552f68a80d7bb05704b6c
Khaii-Htoo <khaihtooag@gmail.com>
2025-11-19 14:18:47 +0530

    svae

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,31 @@
-import express from 'express';
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env.HOST ?? "localhost";
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+app.set("trust proxt", 1);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
 });
 
 app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] http://${host}:${port}`);
 });
